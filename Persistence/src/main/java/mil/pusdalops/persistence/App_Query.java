d9658a80f86ec896c788d62e46d8f703748f310f
mrusli <mrusli@asimetris.co.id>
2020-11-20 13:58:09 +0700

    test commit to git

--- Persistence/src/main/java/mil/pusdalops/persistence/App_Query.java
+++ Persistence/src/main/java/mil/pusdalops/persistence/App_Query.java
@@ -2,10 +2,12 @@ package mil.pusdalops.persistence;
 
 import java.math.BigInteger;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import mil.pusdalops.domain.kejadian.Kejadian;
 import mil.pusdalops.domain.kerugian.Pihak;
 import mil.pusdalops.domain.kerugian.TipeKerugian;
 import mil.pusdalops.persistence.kejadian.rekap.dao.KejadianRekapDao;
@@ -23,7 +25,8 @@ public class App_Query {
 		BigInteger count = kejadianRekapDao.countKerugianByTipe(TipeKerugian.Material, Pihak.KITA, LocalDateTime.now(), LocalDateTime.now());
 		System.out.println(count);
 		
-		// List<Kejadian> kejadianList = kejadianRekapDao.findAllKejadianKerugian(null, null);
+		@SuppressWarnings("unused")
+		List<Kejadian> kejadianList = kejadianRekapDao.findAllKejadian(null, null);
 		
 		// KejadianDao kejadianDao = (KejadianDao) ctx.getBean("kejadianDao");
 		// List<Kejadian> kejadianList = kejadianDao.findAllKejadian(true);
